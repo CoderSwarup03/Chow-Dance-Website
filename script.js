942aa0cb76376e7ae0b19888a6e61e2ea2878171
CoderSwarup03 <swarupmahata321@gmail.com>
2026-01-22 20:11:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+<script>
+    const slider = document.getElementById("slider");
+    const slides = slider.children.length;
+    let index = 0;
+
+    setInterval(() => {
+        index = (index + 1) % slides;
+        slider.style.transform = `translateX(-${index * 100}%)`;
+    }, 4000); // change slide every 4 sec
+</script>
